df77faf7b75d0d4e4daddfa41418d1eaee367f3f
Dumpley <dumpleygames@gmail.com>
2023-12-05 01:05:33 +0100

    Start of the project upload

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <nav>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link> |
+        <router-link to="/register_user">Log In</router-link>
+    </nav>
+    <router-view />
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #ffffff;
+    background-color: #171c22;
+    margin: 0;
+    padding: 5%;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="starting_page">
+        <div class="sider">
+            <Profile></Profile>
+            <Quicklinks></Quicklinks>
+        </div>
+        <div class="post_list">
+            <Post_Overview></Post_Overview>
+        </div>
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Post_Overview from "@/components/Post_Overview.vue";
+import Profile from "@/components/Profile.vue";
+import Quicklinks from "@/components/Quicklinks.vue";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+    name: "HomeView",
+    components: {
+        Profile,
+        Quicklinks,
+        Post_Overview,
+    },
+};
 </script>
+
+<style lang="css">
+.starting_page {
+    display: flex;
+    flex-direction: row;
+}
+.post_list {
+  width: 100%;
+}
+</style>
